e3d1fd685c92383b634ca9f58999650ae467366d
Matias D. Frtiz <fritzmatias@gmail.com>
2021-08-08 19:35:13 -0300

    build: add test folder

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/test'],
   testMatch: [
+    '**/tests/**/*.+(ts|tsx|js)',
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
-    '^.+.(ts|tsx)$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
   collectCoverageFrom: [
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
+    '!**/test/**',
+    '!**/__test__/**',
   ],
   globals: {
     'ts-jest': {
